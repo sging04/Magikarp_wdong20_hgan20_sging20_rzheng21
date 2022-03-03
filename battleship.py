85d2a976435d22ed22c1433b99f5809a0f04ea3c
rengzhengcodes <rzhengcontacts@gmail.com>
2022-03-03 08:28:34 -0500

    new str method

--- battleship.py
+++ battleship.py
@@ -1,9 +1,17 @@
 class Battleship:
 	def __init__(self):
-		self.board1 = [[0 for y in range(7)] for x in range(7)]
-		self.board2 = [[0 for y in range(7)] for x in range(7)]
+		self.boards = {
+			1: [[0 for y in range(7)] for x in range(7)],
+			2: [[0 for y in range(7)] for x in range(7)]
+		}
 
-	def __str__(self):
-		return "{board1} \n {board2}".format(board1=self.board1, board2=self.board2)
+	def __str__(self) -> str:
+		string = ""
 
+		for player, board in self.boards.items():
+			string += f"{player}:\n"
+			for row in board:
+				string += f"{row}\n"
+
+		return string
 print(Battleship())
