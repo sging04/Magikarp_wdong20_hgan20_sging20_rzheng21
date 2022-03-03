9bfa436fae2e5d7550398ee608b0c71421d5f7e8
rengzhengcodes <rzhengcontacts@gmail.com>
2022-03-03 08:35:59 -0500

    rudimentary hit check

--- battleship.py
+++ battleship.py
@@ -5,6 +5,8 @@ class Battleship:
 			2: [[0 for y in range(7)] for x in range(7)]
 		}
 
+		self.ship_sizes = [2, 3, 3, 4, 5]
+
 	def __str__(self) -> str:
 		string = ""
 
@@ -14,4 +16,17 @@ class Battleship:
 				string += f"{row}\n"
 
 		return string
+
+	def hit(player:int, coord:tuple) -> bool:
+		if player not in self.boards:
+			raise KeyError("Player does not exist")
+		elif coord[0] >= len(self.boards[player]) or coord[1] >= len(self.boards[player][0]):
+			return ValueError("Coordinate does not exist")
+		else:
+			if self.boards[player][coord[0]][coord[1]] != 0:
+				self.boards[player][coord[0]][coord[1]] = -self.boards[player][coord[0]][coord[1]]
+				return True
+			else:
+				self.boards[player][coord[0]][coord[1]] = -max(self.ship_sizes) + 1
+				return False
 print(Battleship())
