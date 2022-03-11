1a6d926afdde4ab7bb29021947c9c1786657ef4d
Cabidge <cabbageboi69@gmail.com>
2022-03-11 08:07:18 -0500

    Added more empty routes to main app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, session
+from database import Database
 
 app = Flask(__name__)
 
+db = Database("database.db")
+
 @app.route("/")
 def home():
-  return "Ah"
+    return "Ah"
+
+@app.route("register")
+def register():
+    pass
+
+@app.route("login")
+def register():
+    pass
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
