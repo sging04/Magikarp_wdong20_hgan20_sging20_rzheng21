e49fe7f1dfee1f55ff789c73e715af5cd038f959
Cabidge <cabbageboi69@gmail.com>
2022-03-11 08:34:48 -0500

    Set up basic login page

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,27 @@ def register():
         else:
             return redirect(url_for("register", error="User already exists"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    pass
+    if request.method == "GET":
+        error = request.args.get("error", None)
+        return render_template("login.html", error=error)
+    
+    if request.method == "POST":
+        user = request.form["username"]
+        pwd = request.form["password"]
+
+        db = Database("database.db")
+        user_id = db.fetch_user(user, pwd)
+        db.close()
+
+        print(user_id)
+        if user_id is None:
+            return redirect(url_for("login", error="Incorrect credentials"))
+
+        # TODO: update session to reflect log in
+
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
