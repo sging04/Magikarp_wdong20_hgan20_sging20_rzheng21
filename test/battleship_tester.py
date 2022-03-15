fb2f20fc0708c496408a38e07793b9a7881b786b
rengzhengcodes <rzhengcontacts@gmail.com>
2022-03-15 08:09:23 -0400

    sink works

--- test/battleship_tester.py
+++ test/battleship_tester.py
@@ -62,7 +62,7 @@ def check_ship_hits(tests:int = 100) -> bool:
 		# print(f"pass{i} | Game: {game}")
 	return True
 
-def check_ship_sinks(test:int = 100) -> bool:
+def check_ship_sinks(tests:int = 100) -> bool:
 	"""
 	Tests hitting ships, and sinks.
 	"""
@@ -72,8 +72,17 @@ def check_ship_sinks(test:int = 100) -> bool:
 
 		board = game.return_board_as_array(1)
 
-		for x in range(len(board)):
-			pass
+		for ship in game.players[1]["ships"]:
+			for i in range(len(ship.get_locations())-1):
+				if game.attack(1, ship.get_locations()[i]): #THIS SHOULD ALWAYS BE FALSE IF THE SHIP ISNT SUNK, AND IT ISNT SUNK IF WE DONT HIT THE LAST BIT
+					return False
+				game.attack(0, ship.get_locations()[i]) # passes the turn back
+			if not game.attack(1, ship.get_locations()[-1]): #THIS SHOULD ALWAYS BE FALSE IF THE SHIP IS SUNK
+				return False
+			game.attack(0, ship.get_locations()[-1])#passes the turn back
 
+		# print(i, f"{game}")
+	return True
 print("Board Gen: " + str(check_board_gen()))
 print("Hits: " + str(check_ship_hits()))
+print("Sinks: " + str(check_ship_sinks()))
